8ba2609b5ab14608cd69d7326b980c99eb4bde91
joleaper <joleap123@gmail.com>
2021-01-25 20:10:20 -0800

    all components are rendering successfully

---
+++ src/components/List/ListItem/ListItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+ListItem.propTypes = {
+    
+};
+
+function ListItem(props) {
+    return (
+        <div>
+            This is an Item in the List
+        </div>
+    );
+}
+
+export default ListItem;
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SearchBar(props) {
+    return (
+        <div>
+            This is the search bar
+        </div>
+    );
+}
+
+export default SearchBar;
